1d84068db681bcdfdddc573a3d9facba527e70d6
DESKTOP-PTFEDG1\USER <it.jing.yin@gmail.com>
2024-01-20 01:54:43 +0800

    [Build] - Build a new project

---
+++ slnLoginPagePractice20240117/prjLoginPagePractice20240117/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace prjLoginPagePractice20240117.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
